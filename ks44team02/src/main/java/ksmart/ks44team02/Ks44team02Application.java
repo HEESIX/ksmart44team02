049a074c259c8eb2199501c0e8420e9d738e74f7
HEESIX <jbjjsc447@gmail.com>
2022-09-13 16:14:34 +0900

    [ks44team02] 팀프로젝트 초기셋팅 Author: 장민관

---
+++ ks44team02/src/main/java/ksmart/ks44team02/Ks44team02Application.java
@@ -0,0 +1,13 @@
+package ksmart.ks44team02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ks44team02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ks44team02Application.class, args);
+	}
+
+}
