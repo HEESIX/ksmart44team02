83d3bd921caa9ae028421785e2faf5fb89e50270
HEESIX <jbjjsc447@gmail.com>
2022-09-14 17:30:29 +0900

    [ks44team02] 관리자 상품 컨트롤러 메서드 선언 진행중 Author: 장민관

---
+++ ks44team02/src/main/java/ks44team02/admin/controller/AdminGoodsController.java
@@ -0,0 +1,92 @@
+package ks44team02.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminGoodsController {
+	
+	//상품 카테고리 등록
+	public String addGoodsCategory() {
+		return null;
+	}
+	
+	//상품 카테고리 리스트
+	public String getGoodsCategoryList() {
+		return null;
+	}
+	
+	//상품 카테고리 수정
+	public String modifyGoodsCategory() {
+		return null;
+	}
+	
+	//상품 카테고리 삭제
+	public String removeGoodsCategory() {
+		return null;
+	}
+	
+	//상품 등록
+	public String addGoods() {
+		return null;
+	}
+	
+	//상품 리스트
+	public String getAdminGoodsList() {
+		return null;
+	}
+	
+	//개별 상품 정보
+	public String getGoodsInfo() {
+		return null;
+	}
+	
+	//상품 수정
+	public String modifyGoods() {
+		return null;
+	}
+	
+	//상품 삭제
+	public String removeAdminGoods() {
+		return null;
+	}
+	
+	//식단 등록
+	public String addAdminMenu() {
+		return null;
+	}
+	
+	//식단 리스트
+	public String getAdminMenuList() {
+		return null;
+	}
+	
+	//식단 수정
+	public String modifyAdminMenu() {
+		return null;
+	}
+	
+	//식단 삭제
+	public String removeAdminMenu() {
+		return null;
+	}
+	
+	//개별 식단 정보 
+	public String getMenuInfo() {
+		return null;
+	}
+	
+	//상품별 할인 혜택 등록
+	public String addGoodsDiscount() {
+		return null;
+	}
+	
+	//상품별 할인 혜택 리스트
+	public String getGoodsDiscountList() {
+		return null;
+	}
+	
+	//상품별 할인 혜택 수정
+	public String modifyGoodsDiscount() {
+		return null;
+	}
+}
