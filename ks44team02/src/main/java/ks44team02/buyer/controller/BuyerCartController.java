4eaa0234f05ef5035b4c2681bfb4a5262032434c
jiseok <jiseok0410@naver.com>
2022-09-23 14:09:20 +0900

    [ks44team02] BuyerCartController author: 서지석 detail: 장바구니에담긴상품목록조회메서드,장바구니에담긴상품주문화면으로선택이동메서드,장바구니에담긴상품선택삭제메서드 생성

---
+++ ks44team02/src/main/java/ks44team02/buyer/controller/BuyerCartController.java
@@ -0,0 +1,47 @@
+package ks44team02.buyer.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ks44team02.service.CartService;
+
+@Controller
+@RequestMapping(value = "/buyer/mypage/cart")
+public class BuyerCartController {
+
+	
+	private static final Logger log = LoggerFactory.getLogger(BuyerCartController.class);
+
+	private final CartService cartService;
+	
+	public BuyerCartController(CartService cartService) {
+		this.cartService = cartService;
+	}
+	
+	@PostConstruct
+	public void buyerCartController() {
+		log.info("BuyerCartController bean 생성");
+	}
+	
+	//장바구니에 담긴 상품 목록 조회
+	@GetMapping("/cart_list")
+	public String getCartList() {
+		return "buyer/mypage/cart/cart_list";
+	}
+	//장바구니에 담긴 상품 선택 삭제
+	@PostMapping("/cart_list")
+	public String removeCartGoods() {
+		return "buyer/mypage/cart/cart_list";
+	}
+	//장바구니에 담긴 상품 주문화면으로 선택 이동
+	@PostMapping("/cart_list")
+	public String moveCartGoods() {
+		return "buyer/mypage/cart/cart_list";
+	}
+}
