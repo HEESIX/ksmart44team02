f522c676aa0df8d80e5db656cd45acb6e1e42d2b
jiseok <jiseok0410@naver.com>
2022-09-23 17:26:05 +0900

    [ks44team02] DiscountMapper 작성 author: 서지석

--- ks44team02/src/main/java/ks44team02/mapper/DiscountMapper.java
+++ ks44team02/src/main/java/ks44team02/mapper/DiscountMapper.java
@@ -1,8 +1,26 @@
 package ks44team02.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import ks44team02.dto.OrderDiscount;
 
 @Mapper
 public interface DiscountMapper {
 
+	//주문서별 할인혜택 목록조회
+	public List<OrderDiscount> getOrderDiscountList();
+	
+	//특정 주문서별 할인혜택 조회
+	public OrderDiscount getOrderDiscountInfo();
+		
+	//주문서별 할인혜택 수정
+	public void modifyOrderDiscount(OrderDiscount orderDiscount);
+	
+	//주문서별 할인혜택 등록
+	public void addOrderDiscount(OrderDiscount orderDiscount);
+		
+	//주문서별 할인혜택 삭제
+	public void removeOrderDiscount(OrderDiscount orderDiscount);
 }
