33f0f11695809795bc2a70833ab410e3bc8c6ac7
jiseok <jiseok0410@naver.com>
2022-09-26 17:20:37 +0900

    [ks44team02] mapper쿼리문 기입, dto안에 세터 게터, toString 생성 author: 서지석 file: memberList.html detail: 회원목록 - 어떤식으로 구현(상세하게 기술)

--- ks44team02/src/main/java/ks44team02/dto/OrderDiscount.java
+++ ks44team02/src/main/java/ks44team02/dto/OrderDiscount.java
@@ -1,5 +1,82 @@
 package ks44team02.dto;
 //주문서별 할인혜택
 public class OrderDiscount {
-
+	private String orderDiscountCode;
+	private int discountRate;
+	private int discountPrice;
+	private String discountName;
+	private String discountExpirationDate;
+	private int minOrderPrice;
+	private int maxDiscountPrice;
+	private int gProductionRegDateTime;
+	public String getOrderDiscountCode() {
+		return orderDiscountCode;
+	}
+	public void setOrderDiscountCode(String orderDiscountCode) {
+		this.orderDiscountCode = orderDiscountCode;
+	}
+	public int getDiscountRate() {
+		return discountRate;
+	}
+	public void setDiscountRate(int discountRate) {
+		this.discountRate = discountRate;
+	}
+	public int getDiscountPrice() {
+		return discountPrice;
+	}
+	public void setDiscountPrice(int discountPrice) {
+		this.discountPrice = discountPrice;
+	}
+	public String getDiscountName() {
+		return discountName;
+	}
+	public void setDiscountName(String discountName) {
+		this.discountName = discountName;
+	}
+	public String getDiscountExpirationDate() {
+		return discountExpirationDate;
+	}
+	public void setDiscountExpirationDate(String discountExpirationDate) {
+		this.discountExpirationDate = discountExpirationDate;
+	}
+	public int getMinOrderPrice() {
+		return minOrderPrice;
+	}
+	public void setMinOrderPrice(int minOrderPrice) {
+		this.minOrderPrice = minOrderPrice;
+	}
+	public int getMaxDiscountPrice() {
+		return maxDiscountPrice;
+	}
+	public void setMaxDiscountPrice(int maxDiscountPrice) {
+		this.maxDiscountPrice = maxDiscountPrice;
+	}
+	public int getgProductionRegDateTime() {
+		return gProductionRegDateTime;
+	}
+	public void setgProductionRegDateTime(int gProductionRegDateTime) {
+		this.gProductionRegDateTime = gProductionRegDateTime;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("OrderDiscount [orderDiscountCode=");
+		builder.append(orderDiscountCode);
+		builder.append(", discountRate=");
+		builder.append(discountRate);
+		builder.append(", discountPrice=");
+		builder.append(discountPrice);
+		builder.append(", discountName=");
+		builder.append(discountName);
+		builder.append(", discountExpirationDate=");
+		builder.append(discountExpirationDate);
+		builder.append(", minOrderPrice=");
+		builder.append(minOrderPrice);
+		builder.append(", maxDiscountPrice=");
+		builder.append(maxDiscountPrice);
+		builder.append(", gProductionRegDateTime=");
+		builder.append(gProductionRegDateTime);
+		builder.append("]");
+		return builder.toString();
+	}
 }
