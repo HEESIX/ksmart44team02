816c4b21152eb582d18b5d00b3c9d6b88f0a501a
HEESIX <jbjjsc447@gmail.com>
2022-09-27 13:51:04 +0900

    [ks44team02] 상품 코드 발행 제작 Author: 장민관

--- ks44team02/src/main/java/ks44team02/dto/Goods.java
+++ ks44team02/src/main/java/ks44team02/dto/Goods.java
@@ -3,19 +3,20 @@ package ks44team02.dto;
 public class Goods {
 	private String goodsCode;
 	private String enterCode;
+	private String goodsCateCode;
 	private String goodsName;
 	private int goodsPrice;
 	private String goodsDiscountCode;
-	private int discountPrice;
+	private int goodsDiscountPrice;
+	private String goodsNetWeight;
 	private String goodsProduce;
-	private String goodsProductionDate;
-	private String goodsExpirationDate;
+	private String goodsProductionExpirationDate;
 	private String goodsInfoImage;
 	private String goodsMainImage;
-	private String goodsRegDatetime;
 	private int goodsStock;
 	private int goodsDeliveryCharge;
-	private char goodsActivation;
+	private String goodsRegDatetime;
+	private int goodsActivation;
 	public String getGoodsCode() {
 		return goodsCode;
 	}
@@ -27,6 +28,12 @@ public class Goods {
 	}
 	public void setEnterCode(String enterCode) {
 		this.enterCode = enterCode;
+	}
+	public String getGoodsCateCode() {
+		return goodsCateCode;
+	}
+	public void setGoodsCateCode(String goodsCateCode) {
+		this.goodsCateCode = goodsCateCode;
 	}
 	public String getGoodsName() {
 		return goodsName;
@@ -46,11 +53,17 @@ public class Goods {
 	public void setGoodsDiscountCode(String goodsDiscountCode) {
 		this.goodsDiscountCode = goodsDiscountCode;
 	}
-	public int getDiscountPrice() {
-		return discountPrice;
+	public int getGoodsDiscountPrice() {
+		return goodsDiscountPrice;
 	}
-	public void setDiscountPrice(int discountPrice) {
-		this.discountPrice = discountPrice;
+	public void setGoodsDiscountPrice(int goodsDiscountPrice) {
+		this.goodsDiscountPrice = goodsDiscountPrice;
+	}
+	public String getGoodsNetWeight() {
+		return goodsNetWeight;
+	}
+	public void setGoodsNetWeight(String goodsNetWeight) {
+		this.goodsNetWeight = goodsNetWeight;
 	}
 	public String getGoodsProduce() {
 		return goodsProduce;
@@ -58,17 +71,11 @@ public class Goods {
 	public void setGoodsProduce(String goodsProduce) {
 		this.goodsProduce = goodsProduce;
 	}
-	public String getGoodsProductionDate() {
-		return goodsProductionDate;
+	public String getGoodsProductionExpirationDate() {
+		return goodsProductionExpirationDate;
 	}
-	public void setGoodsProductionDate(String goodsProductionDate) {
-		this.goodsProductionDate = goodsProductionDate;
-	}
-	public String getGoodsExpirationDate() {
-		return goodsExpirationDate;
-	}
-	public void setGoodsExpirationDate(String goodsExpirationDate) {
-		this.goodsExpirationDate = goodsExpirationDate;
+	public void setGoodsProductionExpirationDate(String goodsProductionExpirationDate) {
+		this.goodsProductionExpirationDate = goodsProductionExpirationDate;
 	}
 	public String getGoodsInfoImage() {
 		return goodsInfoImage;
@@ -82,12 +89,6 @@ public class Goods {
 	public void setGoodsMainImage(String goodsMainImage) {
 		this.goodsMainImage = goodsMainImage;
 	}
-	public String getGoodsRegDatetime() {
-		return goodsRegDatetime;
-	}
-	public void setGoodsRegDatetime(String goodsRegDatetime) {
-		this.goodsRegDatetime = goodsRegDatetime;
-	}
 	public int getGoodsStock() {
 		return goodsStock;
 	}
@@ -100,10 +101,16 @@ public class Goods {
 	public void setGoodsDeliveryCharge(int goodsDeliveryCharge) {
 		this.goodsDeliveryCharge = goodsDeliveryCharge;
 	}
-	public char getGoodsActivation() {
+	public String getGoodsRegDatetime() {
+		return goodsRegDatetime;
+	}
+	public void setGoodsRegDatetime(String goodsRegDatetime) {
+		this.goodsRegDatetime = goodsRegDatetime;
+	}
+	public int getGoodsActivation() {
 		return goodsActivation;
 	}
-	public void setGoodsActivation(char goodsActivation) {
+	public void setGoodsActivation(int goodsActivation) {
 		this.goodsActivation = goodsActivation;
 	}
 	@Override
@@ -113,35 +120,36 @@ public class Goods {
 		builder.append(goodsCode);
 		builder.append(", enterCode=");
 		builder.append(enterCode);
+		builder.append(", goodsCateCode=");
+		builder.append(goodsCateCode);
 		builder.append(", goodsName=");
 		builder.append(goodsName);
 		builder.append(", goodsPrice=");
 		builder.append(goodsPrice);
 		builder.append(", goodsDiscountCode=");
 		builder.append(goodsDiscountCode);
-		builder.append(", discountPrice=");
-		builder.append(discountPrice);
+		builder.append(", goodsDiscountPrice=");
+		builder.append(goodsDiscountPrice);
+		builder.append(", goodsNetWeight=");
+		builder.append(goodsNetWeight);
 		builder.append(", goodsProduce=");
 		builder.append(goodsProduce);
-		builder.append(", goodsProductionDate=");
-		builder.append(goodsProductionDate);
-		builder.append(", goodsExpirationDate=");
-		builder.append(goodsExpirationDate);
+		builder.append(", goodsProductionExpirationDate=");
+		builder.append(goodsProductionExpirationDate);
 		builder.append(", goodsInfoImage=");
 		builder.append(goodsInfoImage);
 		builder.append(", goodsMainImage=");
 		builder.append(goodsMainImage);
-		builder.append(", goodsRegDatetime=");
-		builder.append(goodsRegDatetime);
 		builder.append(", goodsStock=");
 		builder.append(goodsStock);
 		builder.append(", goodsDeliveryCharge=");
 		builder.append(goodsDeliveryCharge);
+		builder.append(", goodsRegDatetime=");
+		builder.append(goodsRegDatetime);
 		builder.append(", goodsActivation=");
 		builder.append(goodsActivation);
 		builder.append("]");
 		return builder.toString();
 	}
-	
 	
 }
--- ks44team02/src/main/java/ks44team02/mapper/CommonMapper.java
+++ ks44team02/src/main/java/ks44team02/mapper/CommonMapper.java
@@ -6,4 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface CommonMapper {
 	
 	public boolean sessionIdPwCheck(String memberId, String memberPw);
+	
+	//테이블 명과 컬럼명을 입력하여 새로운 코드를 반환
+	public String getNewCode(String tableName);
 }
