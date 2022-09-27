e809416f1b778e185dfe5f155979eaa72becd335
jain924 <okanev@naver.com>
2022-09-27 09:42:31 +0900

    [ks44team02] author: 황자인 detail: [sellercompany] SellerCompanyController SellerCompanyService SellerCompanyMapper 업체 정보 등록/리스트/수정/정보/삭제 화면
    [sellersales]
    SellerSalesController
    SellerSalesService
    SellerSalesMapper
    판매자 매출 현황/정산 확인 화면
    
    [admin seller company]
    AdminCompanyController
    AdminCompanyService
    AdminCCompanyMapper
    업체 입점 승인 폼/정보 리스트/삭제 승인 폼
    
    [admin calculate]
    AdminCalculateController
    AdminCalculateService
    AdminCalculateMapper
    판매자 일일 정산금 확인/관리자 수수료 확인/관리자 수수료 정산/판매자 정산금 정산

---
+++ ks44team02/src/main/java/ks44team02/service/SellerSalesService.java
@@ -0,0 +1,23 @@
+package ks44team02.service;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "seller/salespage")
+public class SellerSalesService {
+	
+	//판매자 매출 현황 화면
+	@GetMapping("/seller_sales")
+	public String getSaleList() {
+		return "seller/salespage/seller_sales";
+	}
+	
+	//판매자 매출 정산 확인 화면
+	@GetMapping("/seller_sales_calculate")
+	public String sellerSalesSuccess() {
+		return "seller/salespage/seller_sales_calculate";	
+	}
+}
+
