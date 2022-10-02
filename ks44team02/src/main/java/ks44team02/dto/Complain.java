18757de655f1d0448e0d5a326f90df20548c9ad0
User <User@DESKTOP-8QP28T1>
2022-10-02 19:23:35 +0900

    [ks44team02]
    author : 서동균
    contents : adminComplainController수정
    Complain dto 수정
    ComplainMapper수정

--- ks44team02/src/main/java/ks44team02/dto/Complain.java
+++ ks44team02/src/main/java/ks44team02/dto/Complain.java
@@ -2,22 +2,46 @@ package ks44team02.dto;
 
 //신고내역 구성요소,내용(객체)
 public class Complain {
+	private String complainCode;
 	private String memberId;
+	private String gCode;
 	private String complainCategory;
+	private String enterCode;
 	private String complainTitle;
 	private String complainContent;
-	private String complainRegDate;
+	private String complainRegDatetime;
+	private char complainAnswerConfirm;
+	private String complainAnswerContent;
+	private String complainAnswerDatetime;
+	public String getComplainCode() {
+		return complainCode;
+	}
+	public void setComplainCode(String complainCode) {
+		this.complainCode = complainCode;
+	}
 	public String getMemberId() {
 		return memberId;
 	}
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
+	public String getgCode() {
+		return gCode;
+	}
+	public void setgCode(String gCode) {
+		this.gCode = gCode;
+	}
 	public String getComplainCategory() {
 		return complainCategory;
 	}
 	public void setComplainCategory(String complainCategory) {
 		this.complainCategory = complainCategory;
+	}
+	public String getEnterCode() {
+		return enterCode;
+	}
+	public void setEnterCode(String enterCode) {
+		this.enterCode = enterCode;
 	}
 	public String getComplainTitle() {
 		return complainTitle;
@@ -31,28 +55,60 @@ public class Complain {
 	public void setComplainContent(String complainContent) {
 		this.complainContent = complainContent;
 	}
-	public String getComplainRegDate() {
-		return complainRegDate;
+	public String getComplainRegDatetime() {
+		return complainRegDatetime;
 	}
-	public void setComplainRegDate(String complainRegDate) {
-		this.complainRegDate = complainRegDate;
+	public void setComplainRegDatetime(String complainRegDatetime) {
+		this.complainRegDatetime = complainRegDatetime;
+	}
+	public char getComplainAnswerConfirm() {
+		return complainAnswerConfirm;
+	}
+	public void setComplainAnswerConfirm(char complainAnswerConfirm) {
+		this.complainAnswerConfirm = complainAnswerConfirm;
+	}
+	public String getComplainAnswerContent() {
+		return complainAnswerContent;
+	}
+	public void setComplainAnswerContent(String complainAnswerContent) {
+		this.complainAnswerContent = complainAnswerContent;
+	}
+	public String getComplainAnswerDatetime() {
+		return complainAnswerDatetime;
+	}
+	public void setComplainAnswerDatetime(String complainAnswerDatetime) {
+		this.complainAnswerDatetime = complainAnswerDatetime;
 	}
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Complain [memberId=");
+		builder.append("Complain [complainCode=");
+		builder.append(complainCode);
+		builder.append(", memberId=");
 		builder.append(memberId);
+		builder.append(", gCode=");
+		builder.append(gCode);
 		builder.append(", complainCategory=");
 		builder.append(complainCategory);
+		builder.append(", enterCode=");
+		builder.append(enterCode);
 		builder.append(", complainTitle=");
 		builder.append(complainTitle);
 		builder.append(", complainContent=");
 		builder.append(complainContent);
-		builder.append(", complainRegDate=");
-		builder.append(complainRegDate);
+		builder.append(", complainRegDatetime=");
+		builder.append(complainRegDatetime);
+		builder.append(", complainAnswerConfirm=");
+		builder.append(complainAnswerConfirm);
+		builder.append(", complainAnswerContent=");
+		builder.append(complainAnswerContent);
+		builder.append(", complainAnswerDatetime=");
+		builder.append(complainAnswerDatetime);
 		builder.append("]");
 		return builder.toString();
 	}
+	
+	
 	
 	
 	
--- ks44team02/src/main/java/ks44team02/mapper/ComplainMapper.java
+++ ks44team02/src/main/java/ks44team02/mapper/ComplainMapper.java
@@ -1,13 +1,16 @@
 package ks44team02.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import ks44team02.dto.Complain;
 
 @Mapper
 public interface ComplainMapper {
 	
 	
   // 회원 Complain 내역 조회
-  public String getComplainList();
+  public List<Complain> getComplainList();
 	
   // 회원 Complain 삭제
   public int ComplainDelete();
