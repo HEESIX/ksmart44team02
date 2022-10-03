9234a5764e1f9bab8b13b23944c5ee17b8b5d4e0
User <User@DESKTOP-8QP28T1>
2022-10-03 18:43:12 +0900

    [ks44team02]
    author : 서동균
    contents : AdminPenaltycController 패널티 부여,패널티 부여 처리 매서드 작성
    mapper,service 작성

--- ks44team02/src/main/java/ks44team02/service/PenaltyService.java
+++ ks44team02/src/main/java/ks44team02/service/PenaltyService.java
@@ -2,6 +2,9 @@ package ks44team02.service;
 
 import org.springframework.stereotype.Service;
 
+import ks44team02.admin.controller.AdminPenaltyController;
+import ks44team02.dto.Complain;
+import ks44team02.dto.Penalty;
 import ks44team02.mapper.PenaltyMapper;
 
 @Service
@@ -21,6 +24,17 @@ public class PenaltyService {
 	//관리자 패널티 부여
 	public int givePenalty() {
 		return 0;
+	}
+
+	public Penalty getPenaltyInfo(String penaltyStateCode) {
+		Penalty PanaltyInfo = penaltyMapper.getPenaltyInfo();
+		return null;
+	}
+
+	public boolean givePenalty(Penalty penalty) {
+		boolean result = penaltyMapper.givePenalty(penalty);
+		return result;
+
 	}
 
 }
