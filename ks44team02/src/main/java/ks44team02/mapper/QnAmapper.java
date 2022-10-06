77fcd9bd17155e21cd942efb001317e96737cbdc
seodg <seodg@59.1.107.173>
2022-10-06 21:34:52 +0900

    [ks44team02] 구매자 문의 관련 컨트롤러 작성
    author : 서동균
    contents: 구매자 문의조회, 문의 수정, 문의 삭제, 수정처리, 삭제처리 컨트롤러 작성

--- ks44team02/src/main/java/ks44team02/mapper/QnAmapper.java
+++ ks44team02/src/main/java/ks44team02/mapper/QnAmapper.java
@@ -2,12 +2,39 @@ package ks44team02.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ks44team02.dto.QnA;
+
 @Mapper
 public interface QnAmapper {
 
-	//회원 QnA 내역 조회
+	//회원 QnA 내역 조회(관리자)
 	public String getqnaList();
 	
-	//회원 QnA 답글 등록
+	//회원 QnA 답글 등록(관리자)
 	public int qnaAnswer();
+	
+	// 판매자 문의 조회
+		public String getSellerInqueryList();
+
+		// 판매자 문의 답변
+		public int answerInquery();
+
+		// 구매자 문의 조회
+		public String getBuyerInqueryList();
+
+		// 구매자 문의 등록
+		public int regBuyerInquery();
+
+		// 구매자 문의 수정
+		public int modifyBuyerInquery();
+
+		// 구매자 문의 삭제
+		public int deleteBuyerInquery();
+
+		public boolean regBuyerInquery(QnA qna);
+
+		public boolean modifyBuyerInquery(QnA qna);
+
+		public boolean deleteBuyerInquery(String qnaCode);
+
 }
--- ks44team02/src/main/java/ks44team02/service/QnAservice.java
+++ ks44team02/src/main/java/ks44team02/service/QnAservice.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import ks44team02.dto.MemberMileageAcc;
 import ks44team02.dto.QnA;
 import ks44team02.mapper.QnAmapper;
 
@@ -37,6 +38,58 @@ import ks44team02.mapper.QnAmapper;
 			// TODO Auto-generated method stub
 			return false;
 		}
+		
+		//판매자 문의 조회
+				public String getSellerInqueryList() {
+					return null;
+				}
+					
+				//판매자 문의 답변
+				public int answerInquery() {
+					return 0;
+				}
+			
+				//구매자 문의 조회
+				public List<QnA> getBuyerInqueryList() {
+					return null;
+				}
+				
+					//구매자 문의 수정
+					public int modifyBuyerInquery() {
+						return 0;
+					}
+					//구매자 문의 삭제
+					public int deleteBuyerInquery() {
+						return 0;
+					}
+
+					public QnA getQnAInfo(String qnaContents, String qnaTitle) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					//구매자 문의 등록
+					public boolean regBuyerInquery(QnA qna) {
+						boolean result = qnaMapper.regBuyerInquery(qna);
+						return result;
+					}
+					
+					//구매자 문의수정
+					public boolean modifyBuyerInquery(QnA qna) {
+						boolean result = qnaMapper.modifyBuyerInquery(qna);
+						return result;
+					}
+
+					public boolean deleteBuyerInquery(String qnaCode) {
+						boolean result = qnaMapper.deleteBuyerInquery(qnaCode);
+						return result;
+						
+					}
+
+				
+
+			
+				
 
 	
 	}
