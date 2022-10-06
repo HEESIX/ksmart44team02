2bddb497e84619f296f7739834e8958bc345c458
jain924 <okanev@naver.com>
2022-10-06 17:34:25 +0900

    [ks44team02] 관리자 입점 업체 리스트 author: 황자인

--- ks44team02/src/main/java/ks44team02/admin/controller/AdminCompanyController.java
+++ ks44team02/src/main/java/ks44team02/admin/controller/AdminCompanyController.java
@@ -1,11 +1,18 @@
 package ks44team02.admin.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import ks44team02.dto.Launching;
 import ks44team02.service.AdminCompanyService;
 
 @Controller
@@ -28,7 +35,15 @@ public class AdminCompanyController {
 	
 	//입점 업체 정보 및 리스트
 	@GetMapping("/sellerCompanyList")
-	public String companyInfoList() {
+	public String companyInfoList	(Model model
+									,@RequestParam(value = "sk", required = false) String sk
+									,@RequestParam(value = "sv", required = false) String sv) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put(sk, sv);
+		List<Launching> sellerCompanyList = adminCompanyService.companyInfoList(map);
+		log.info(">>>>>>>>>>>> {}", sellerCompanyList);
+		model.addAttribute("title", "회원정보리스트");
+		model.addAttribute("sellerCompanyList", sellerCompanyList);
 		return "admin/companypage/sellerCompanyList";
 	}
 	
