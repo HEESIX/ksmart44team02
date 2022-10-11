ef794b85a882784edbb56945d53d484c959b112c
jiseok <jiseok0410@naver.com>
2022-10-11 17:26:39 +0900

    [ks44team02] 장바구니 목록 조회 기능 추가 author: 서지석 file: BuyerCartController.java

--- ks44team02/src/main/java/ks44team02/buyer/controller/BuyerCartController.java
+++ ks44team02/src/main/java/ks44team02/buyer/controller/BuyerCartController.java
@@ -1,14 +1,21 @@
 package ks44team02.buyer.controller;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import ks44team02.dto.Cart;
+import ks44team02.dto.OrderDiscount;
 import ks44team02.service.CartService;
 
 @Controller
@@ -31,7 +38,25 @@ public class BuyerCartController {
 	
 	//장바구니에 담긴 상품 목록 조회
 	@GetMapping("/cartList")
-	public String getCartList() {
+	public String getCartList(Model model
+							 ,HttpSession session
+							 ,@RequestParam(value = "msg", required = false) String msg) {
+		
+		String sessionId = (String) session.getAttribute("SID");
+		
+		//세션이 생기면 아래 조건문은 주석처리 해야함
+		if(sessionId == null)  sessionId = "id002";
+		
+		List<Cart> cartList = cartService.getCartList(sessionId);
+		
+		System.out.println(cartList.toString());
+		
+		model.addAttribute("title","장바구니 목록 조회");
+		model.addAttribute("cartList","cartList");
+		
+		if (msg != null)
+			model.addAttribute("msg", msg);
+		
 		return "buyer/mypage/cart/cartList";
 	}
 	//장바구니에 담긴 상품 선택 삭제
