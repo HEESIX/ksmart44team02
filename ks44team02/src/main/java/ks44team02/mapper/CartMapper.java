3fd0b9c0210b6b8b74c1e912b515fd026e4589cf
jiseok <jiseok0410@naver.com>
2022-10-12 17:38:17 +0900

    [ks44team02] 장바구니 상품 삭제 처리 메서드 입력값 수정 author: 서지석 file: CartMapper.java

--- ks44team02/src/main/java/ks44team02/mapper/CartMapper.java
+++ ks44team02/src/main/java/ks44team02/mapper/CartMapper.java
@@ -13,5 +13,5 @@ public interface CartMapper {
 	public List<Cart> getCartList(String sessionId);
 	
 	//장바구니 상품 삭제 처리
-	public void removeCartGoods(String cartListCode);
+	public boolean removeCartGoods(Cart cart);
 }
