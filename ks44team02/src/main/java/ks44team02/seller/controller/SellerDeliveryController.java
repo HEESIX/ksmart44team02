958130e9ae93e0891fd852a84e40950ab62df966
mycatisrockstar1 <bk22019@naver.com>
2022-10-12 15:26:43 +0900

    [ks44team02] 운송장등록 모달창 ajax author: 장보경

--- ks44team02/src/main/java/ks44team02/seller/controller/SellerDeliveryController.java
+++ ks44team02/src/main/java/ks44team02/seller/controller/SellerDeliveryController.java
@@ -15,10 +15,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import ks44team02.dto.Delivery;
 import ks44team02.dto.OrderDetail;
 import ks44team02.dto.OrderStatus;
 import ks44team02.dto.OrderStatusStandard;
@@ -122,8 +125,24 @@ public class SellerDeliveryController {
 	}
 	
 	
-
 	
+	//주문서 운송장번호 등록
+	@PostMapping("/addDeliNumber")
+	@ResponseBody
+	public String addDeliNumber(Delivery delivery,RedirectAttributes reAttr) {
+		log.info(">>>>>>>>>검색{}", delivery);
+		
+		boolean result = deliveryservice.addDeliNumber(delivery);
+		if(result) {
+			reAttr.addAttribute("msg", "수정 완료");
+		}else {
+			reAttr.addAttribute("msg", "수정 실패");
+		}
+		return  "redirect:/seller/order/sellerOrderDeliveryAllList";
+	}
+	
+
+
 	//구매자 주문배송처리 폼
 	@GetMapping("/sellerOrderStatusDeal/")
 	public String OrderDeal(Model model) {
